13b8773a098df3a8694474071df43bd1822e543f
Ramon Marques <ramonlmarques@hotmail.com>
2023-11-15 15:59:48 -0300

    att @CrossOrigin

--- src/main/java/com/example/apibank/config/CorsConfiguration.java
+++ src/main/java/com/example/apibank/config/CorsConfiguration.java
@@ -5,13 +5,16 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
+
 
