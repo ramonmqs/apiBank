a1d78ebc4459782a7a4ebefd5bdec6a7ca7b0bfe
Ramon Marques <128409007+ramonmqs@users.noreply.github.com>
2023-11-15 16:37:07 -0300

    Update CorsConfiguration.java

--- src/main/java/com/example/apibank/config/CorsConfiguration.java
+++ src/main/java/com/example/apibank/config/CorsConfiguration.java
@@ -11,7 +11,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
 
     }
 }
